3af7a867e5e39ffd995bb1795a8ab0dad225b3d2
Chaudhary Sarimurrab <35756749+sarimurrab@users.noreply.github.com>
2020-06-27 23:43:55 +0530

    py mini project

---
+++ Preq 1 Py Mini Project/take_break.py
@@ -0,0 +1,7 @@
+import webbrowser
+import time
+
+for i in range(3):
+    print("Program Started on {}".format(time.ctime()))
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=D2UWNF8pKwk")
